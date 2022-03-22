6d3ce80618533d6641379c55d7ca3437ae470e5e
Amy <jaimes978@outlook.com>
2022-03-21 23:06:40 -0400

    Add answers

--- next.config.js
+++ next.config.js
@@ -2,5 +2,12 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_staticImage: true,
+  redirects: [
+    {
+      source: '/hello-vercel',
+      destination: 'https://vercel.com',
+      permanent: true,
+    },
+  ],
 })
 module.exports = withNextra()

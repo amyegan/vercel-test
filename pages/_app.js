3a04851b706b39fb28f4a52315d2cceb2e2c9b0f
Amy Egan <8247105+amyegan@users.noreply.github.com>
2022-07-25 17:27:22 -0400

    remove comment

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import 'nextra-theme-docs/style.css'
 export default function Nextra({ Component, pageProps }) {
   return (
     <Component {...pageProps} />
-    <!-- Cloudflare Web Analytics -->
     <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "9aa5387698cd4a1793172da9f6f21040"}'></script>
   )
 }

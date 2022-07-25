7cab8b13d204a80b3f474a5bdd2113cf8c010fcc
Amy Egan <8247105+amyegan@users.noreply.github.com>
2022-07-25 17:26:24 -0400

    add cloudflare tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import 'nextra-theme-docs/style.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} />
+    <!-- Cloudflare Web Analytics -->
+    <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "9aa5387698cd4a1793172da9f6f21040"}'></script>
+  )
 }

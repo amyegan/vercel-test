2a13d612642c8636d3cbe27128d2a2391d9f3c8c
Amy Egan <8247105+amyegan@users.noreply.github.com>
2022-07-25 17:39:23 -0400

    append tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import 'nextra-theme-docs/style.css'
 
+useEffect(() => {
+  const script = document.createElement('script');
+
+  script.src = "https://static.cloudflareinsights.com/beacon.min.js";
+  script.data-cf-beacon = '{"token": "9aa5387698cd4a1793172da9f6f21040"}';
+
+  document.body.appendChild(script);
+}, []);
+
 export default function Nextra({ Component, pageProps }) {
   return (
     <Component {...pageProps} />

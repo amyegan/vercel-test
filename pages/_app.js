e10ba44372ed55d09460f9f9b2af64173b75d855
Amy Egan <8247105+amyegan@users.noreply.github.com>
2022-11-15 15:11:27 -0500

    add audience analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import 'nextra-theme-docs/style.css'
+import { Analytics } from '@vercel/analytics/react';
 
 useEffect(() => {
   const script = document.createElement('script');
@@ -12,6 +13,7 @@ useEffect(() => {
 export default function Nextra({ Component, pageProps }) {
   return (
     <Component {...pageProps} />
+    <Analytics />
     <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "9aa5387698cd4a1793172da9f6f21040"}'></script>
   )
 }

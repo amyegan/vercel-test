58c82a80b094d2caa7bee861af10d3ecdb7f1cd4
Amy Egan <8247105+amyegan@users.noreply.github.com>
2022-12-06 09:22:13 -0500

    silence github comment

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,16 @@
 import 'nextra-theme-docs/style.css'
-import { Analytics } from '@vercel/analytics/react';
-
-useEffect(() => {
-  const script = document.createElement('script');
-
-  script.src = "https://static.cloudflareinsights.com/beacon.min.js";
-  script.data-cf-beacon = '{"token": "9aa5387698cd4a1793172da9f6f21040"}';
-
-  document.body.appendChild(script);
-}, []);
+import { Analytics } from '@vercel/analytics/react'
 
 export default function Nextra({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
-    <Analytics />
-    <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "9aa5387698cd4a1793172da9f6f21040"}'></script>
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+      <script
+        defer
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "9aa5387698cd4a1793172da9f6f21040"}'
+      ></script>
+    </>
   )
 }

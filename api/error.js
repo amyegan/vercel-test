798ecc1eb0665d37b07cec1b72b6dcbf1dfd1bfc
Amy Egan <8247105+amyegan@users.noreply.github.com>
2023-01-31 14:14:01 -0500

    add error function

--- api/error.js
+++ api/error.js
@@ -1,5 +1,5 @@
 export default function handler(request, response) {
   response.status(413).json({
-    message: "Failed to post handler success response"
-  });
+    message: err.message,
+  })
 }

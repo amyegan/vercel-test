476ef1d39c88a28fe4e8c99862392a1278ee99ac
Amy Egan <8247105+amyegan@users.noreply.github.com>
2023-01-31 14:17:49 -0500

    Revert "add error function"
    This reverts commit 798ecc1eb0665d37b07cec1b72b6dcbf1dfd1bfc.

--- api/error.js
+++ api/error.js
@@ -1,5 +1,5 @@
 export default function handler(request, response) {
   response.status(413).json({
-    message: err.message,
-  })
+    message: "Failed to post handler success response"
+  });
 }

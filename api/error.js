74605a7deb5fab015d3edd0745239d1b1edf936e
Amy Egan <8247105+amyegan@users.noreply.github.com>
2023-02-14 13:23:31 -0500

    something

--- api/error.js
+++ api/error.js
@@ -1,5 +1,14 @@
+const fetch = require('node-fetch');
+
 export default function handler(request, response) {
-  response.status(413).json({
-    message: "Failed to post handler success response"
-  });
+  try {
+    fetch('http://localhost:3000/api/nothing')
+  } catch (error) {
+    console.log('error', error)
+    console.log('message', error.message)
+    console.log('status', error.status)
+    response.status(413).json({
+      message: 'Failed to post handler success response',
+    })
+  }
 }
